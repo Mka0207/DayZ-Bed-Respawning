174aab73b2facdd85c1c80d86453104299735ea7
Mka0207 <mka0207@fwkzt.com>
2020-09-28 14:25:02 -0400

    Create init.c

---
+++ MissionFileExample/init.c
@@ -0,0 +1,97 @@
+/**
+ * init.c
+ *
+ * DayZ Expansion Mod
+ * www.dayzexpansion.com
+ * © 2020 DayZ Expansion Mod Team
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ *
+*/
+
+#include "$CurrentDir:\\mpmissions\\Expansion.ChernarusPlus\\expansion\\ExpansionObjectSpawnTools.c"
+#include "$CurrentDir:\\mpmissions\\Expansion.ChernarusPlus\\expansion\\missions\\MissionConstructor.c"
+
+void main()
+{
+	bool loadTraderObjects = false;
+	bool loadTraderNPCs = false;
+
+	string MissionWorldName = "empty";
+	GetGame().GetWorldName(MissionWorldName);
+
+	if (MissionWorldName != "empty")
+	{
+		//! Spawn mission objects and traders
+		FindMissionFiles(MissionWorldName, loadTraderObjects, loadTraderNPCs);
+	}
+
+	//INIT WEATHER BEFORE ECONOMY INIT------------------------
+	Weather weather = g_Game.GetWeather();
+
+	weather.MissionWeather(false);	// false = use weather controller from Weather.c
+
+	weather.GetOvercast().Set( Math.RandomFloatInclusive(0.02, 0.1), 1, 0);
+	weather.GetRain().Set( 0, 1, 0);
+	weather.GetFog().Set( 0, 1, 0);
+
+	//INIT ECONOMY--------------------------------------
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
+
+	//DATE RESET AFTER ECONOMY INIT-------------------------
+	int year, month, day, hour, minute;
+	int reset_month = 8, reset_day = 10;
+	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+
+	if ((month == reset_month) && (day < reset_day))
+	{
+		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+	}
+	else
+	{
+		if ((month == reset_month + 1) && (day > reset_day))
+		{
+			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+		}
+		else
+		{
+			if ((month < reset_month) || (month > reset_month + 1))
+			{
+				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+			}
+		}
+	}
+}
+
+/**@class		CustomExpansionMission
+ * @brief		This class handle expansion serverside mission
+ **/
+class CustomMission: MissionServer
+{	
+	override void OnInit()
+	{
+		ExpansionMissionModule missionModule;
+		if ( Class.CastTo( missionModule, GetModuleManager().GetModule( ExpansionMissionModule ) ) )
+		{
+			missionModule.SetMissionConstructor( COMMissionConstructor );
+		}
+
+		super.OnInit();
+	}
+	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	{
+		super.StartingEquipSetup(player,clothesChosen);
+
+		PlayerIdentity id = m_player.GetIdentity();
+		BedFrameWork.FixSpawningHeight( m_player, BedFrameWork.AttemptBedSpawn( id,m_player.GetPosition() ) );
+		BedFrameWork.BreakOldSpawnBed( id, BedFrameWork.AttemptBedSpawn( id,m_player.GetPosition() ) );
+	}
+}
+
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
+}
