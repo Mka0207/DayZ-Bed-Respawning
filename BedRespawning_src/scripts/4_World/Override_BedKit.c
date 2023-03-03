a70acaf534d2a87c77afa9b1ee9e09593d6b6464
Mka0207 <7522605+Mka0207@users.noreply.github.com>
2023-03-03 06:21:27 -0500

    Support for max uses on beds.

--- BedRespawning_src/scripts/4_World/Override_BedKit.c
+++ BedRespawning_src/scripts/4_World/Override_BedKit.c
@@ -50,6 +50,7 @@
 modded class OP_SleepingBagColorbase //modded class TentBase extends ItemBase
 {
 	string m_OwnerID;
+	int m_Uses;
 
 	override void OnPlacementComplete( Man player, vector position = "0 0 0", vector orientation = "0 0 0" )
 	{
@@ -63,12 +64,14 @@ modded class OP_SleepingBagColorbase //modded class TentBase extends ItemBase
 			PlayerIdentity pd = player_base.GetIdentity();
 			m_OwnerID = pd.GetId();
 
-			if ( BedFrameWork.m_BedConfig.EnableBagCoolDown == 1 )
+			m_Uses = BedFrameWork.m_BedConfig.MaxRespawnsBeforeRemoval;
+
+			if ( BedFrameWork.m_BedConfig.BedRespawnTimeMinutes > 0 )
 			{
 				player_base.RPCSingleParam( ERPCs.RPC_USER_ACTION_MESSAGE, new Param1<string>( "Bed-Respawn Cooldown after Death = "+BedFrameWork.m_BedConfig.BedRespawnTimeMinutes+" Minutes." ), true, pd );
 			}
 
-			ref BedData bed = new BedData(m_OwnerID,pos,0);
+			ref BedData bed = new BedData(m_OwnerID,pos,0,m_Uses);
 			BedFrameWork.InsertBed( bed );
 		}
 	}
@@ -78,6 +81,7 @@ modded class OP_SleepingBagColorbase //modded class TentBase extends ItemBase
 		super.OnStoreSave(ctx);
 
 		ctx.Write(m_OwnerID);
+		ctx.Write(m_Uses);
 	}
 
 	override bool OnStoreLoad(ParamsReadContext ctx, int version)
@@ -85,8 +89,10 @@ modded class OP_SleepingBagColorbase //modded class TentBase extends ItemBase
 		if (!super.OnStoreLoad(ctx, version))
         return false;
 
-		// read data loaded from game database (format and order of reading must be the same as writing!)
 		if ( !ctx.Read(m_OwnerID) )
+			return false;
+
+		if ( !ctx.Read(m_Uses) )
 			return false;
 
 		return true;
@@ -128,12 +134,14 @@ class BedData : BedFrameWork
 	string m_BedOwner = "test";
 	vector m_BedPos = "1 0 1";
 	int m_RespawnTime = 0;
+	int m_UsesLeft = 0;
 
-	void BedData(string owner, vector pos, int time)
+	void BedData(string owner, vector pos, int time, int uses)
 	{
 		m_BedOwner = owner;
 		m_BedPos = pos;
 		m_RespawnTime = time;
+		m_UsesLeft = uses;
 	}
 	string GetOwner()
 	{
@@ -147,18 +155,24 @@ class BedData : BedFrameWork
 	{
 		return m_RespawnTime;
 	}
-
 	void SetRespawnTime(int time)
 	{
 		m_RespawnTime = time;
+	}
+	int GetUsesLeft()
+	{
+		return m_UsesLeft;
+	}
+	void SetUsesLeft(int uses)
+	{
+		m_UsesLeft = uses;
 	}
 }
 
 class BedConfig
 {
-	int DestroyBedAfterSpawn = 1;
+	int MaxRespawnsBeforeRemoval = 0;
 	int EnableOPBaseItems_SleepingBags = 1;
-	int EnableBagCoolDown = 1;
 	int BedRespawnTimeMinutes = 45;
 }
 
@@ -238,14 +252,20 @@ class BedFrameWork : Managed
 			string saved_bed = m_DataFolder + identity.GetId() + ".json"; 
 			if ( FileExist(saved_bed) )
 			{
-				BedData bed = new BedData("na","1 0 1", 0);
+				BedData bed = new BedData("na","1 0 1", 0, 0);
 				JsonFileLoader<BedData>.JsonLoadFile(saved_bed, bed);
 				m_SpawnPos = bed.GetPos();
-
-				if ( m_BedConfig.EnableBagCoolDown == 1 && CF_Date.Now(true).GetTimestamp() < bed.GetRespawnTime() )
-				return;
 				
-				if ( m_BedConfig.EnableBagCoolDown == 1 )
+				if ( m_BedConfig.MaxRespawnsBeforeRemoval > 0 )
+				{
+					bed.SetUsesLeft( Math.Clamp( bed.GetUsesLeft() - 1, 0, m_BedConfig.MaxRespawnsBeforeRemoval ) )
+					if ( bed.GetUsesLeft() <= 0 )
+					{
+						BreakOldSpawnBed(identity, m_SpawnPos);
+					}
+				}
+
+				if ( m_BedConfig.BedRespawnTimeMinutes > 0 && CF_Date.Now(true).GetTimestamp() >= bed.GetRespawnTime() )
 				{
 					
 					bed.SetRespawnTime( CF_Date.Now(true).GetTimestamp() + ( BedFrameWork.m_BedConfig.BedRespawnTimeMinutes * 60 ) );
@@ -253,11 +273,6 @@ class BedFrameWork : Managed
 				}
 
 				m_player.SetPosition( m_SpawnPos );
-				
-				if ( m_BedConfig.DestroyBedAfterSpawn == 1 )
-				{
-					BreakOldSpawnBed(identity, m_SpawnPos);
-				}
 			}
 		}
 	}
